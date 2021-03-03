def373f00c84f0943d0e21bbe69341c86b3b6ed0
Marcelino Sandroni <marcelino.sandroni@gmail.com>
2021-03-03 14:22:11 -0300

    update

--- semana4/aula15-planner/index.js
+++ semana4/aula15-planner/index.js
@@ -12,32 +12,32 @@ taskName.focus()
 
 const createTaskOnBoard = (event) => {
 	const weekBox = event.target.parentElement
+	console.log(`Box: ${weekBox}`)
 	const task = document.createElement('input')
 	task.type = 'text'
 	weekBox.insertBefore(task, event.target)
 	task.focus()
 
 	task.onblur = verifyTaskValue
-	//task.onkeydown = verifyTaskValue
-
+	task.onkeydown = verifyTaskValue
 }
 
 verifyTaskValue = (event) => {
-	const task = event.target
-	if(task.value) {
-		console.log("tentando inserir essa task")
-		console.log(`${task.value}`)
-		const newTask= document.createElement('div')
-		newTask.classList.add('task')
-		newTask.textContent = `${task.value} ${time.value}`
-		console.log(task)
-		console.log(task.parentElement)
-		task.parentElement.insertBefore(task, newTask)
-		task.remove()
+	if(event.type === 'blur' || event.type === 'keydown' && event.key === 'Enter') {
+		const task = event.target
 
-	} else {
-		console.log(`Sem dados, removendo INPUT`)
-		task.remove()
+		if(task.value) {
+			console.log(task)
+			const newTask= document.createElement('div')
+			newTask.classList.add('task')
+			newTask.textContent = `${task.value} ${time.value}`
+			task.parentElement.insertBefore(newTask, task)
+
+			task.remove()
+			if(event.type === 'keydown') {
+				createTaskOnBoard(event)
+			}
+		}
 	}
 }
 
@@ -51,24 +51,40 @@ weekBox.forEach(week => {
 	weekAddButton.onclick = createTaskOnBoard
 })
 
+naturalLanguage = text => {
+	let task
+	let week
+	let time
+	let regexTime = /(\w{1,3}\s\d{1,2}$)|(\d{1,2}\w{1,3}$)/g
+
+	let weekDays = ['domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado']
+	let weekIndex = {domingo: 0, segunda: 1, terca: 2, quarta: 3, quinta: 4, sexta: 5, sabado: 6}
+
+	time = text.match(regexTime)
+	console.log(time)
+	text
+
+	text.match(/\w\w/gi)
+	
+}
+
 addButton.onclick = (event) => {
 	event.preventDefault()
 	taskName.value = taskName.value.trim()
 
 	if(taskName.value && weekDay.value) {
+		naturalLanguage(taskName.value)
 		const box = weekBox[weekDay.value]
 		let task = document.createElement('div')
 		task.classList.add('task')
 		task.textContent = `${taskName.value} ${time.value}hrs`
 
-		box.append(task)
 		box.insertBefore(task, box.querySelector('.week-addbutton'))
 		taskName.value = ''
 		taskName.placeholder = 'Digite mais tarefinhas...'
 	} else {
 		alert('Por digite algo né...\nE escolha o dia da semana.\nsou bobo não!')
 	}
-	
 }
 
 
